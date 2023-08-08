8754f25c23901b0e34a231c6f8bcb543dc4cf3ff
Gabriel Sens <gabriel_sens@hotmail.com>
2023-08-07 23:48:17 -0300

    AuthLayout

--- src/view/layouts/AuthLayout.tsx
+++ src/view/layouts/AuthLayout.tsx
@@ -1,3 +1,5 @@
+import { Logo } from '../components/Logo';
+
 import ilustration from '../../assets/ilustration.png';
 
 import { Outlet } from "react-router-dom";
@@ -5,16 +7,22 @@ import { Outlet } from "react-router-dom";
 export function AuthLayout() {
   return (
     <div className="flex w-full h-full">
+      <div className="w-full h-full flex items-center justify-center flex-col gap-16 lg:w-1/2">
+        <Logo className='h-6 text-gray-500 px-8' />
 
-      <div className="bg-red-500 w-1/2 h-full "></div>
+        <div className='w-full max-w-[504px] '>
+          <Outlet />
+        </div>
+      </div>
 
-      <div className="w-1/2 h-full flex justify-center items-center p-8 relative">
+      <div className="w-1/2 h-full justify-center items-center p-8 relative hidden lg:flex">
         <img
           src={ilustration}
           className='object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-[32px]'
         />
-        <div className='max-w-[656px] bottom-2 bg-white p-10 rounded-b-[32px] absolute '>
-          <p>
+        <div className='max-w-[656px] bottom-2 bg-white p-10 rounded-b-[32px] absolute'>
+          <Logo className='text-teal-900 h-8' />
+          <p className='mt-6 text-gray-700 font-medium text-xl'>
             Gerencie suas finanças pessoais de uma forma simples com o fincheck, e o melhor, totalmente de graça!
           </p>
         </div>
